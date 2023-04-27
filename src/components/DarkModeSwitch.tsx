6ebcfb5d8cd6bb37d58e075e9c0d4e83d2f28c40
Hiroya Shitogiden <dende.capo@gmail.com>
2023-04-27 14:50:34 +0900

    first commit

---
+++ src/components/DarkModeSwitch.tsx
@@ -0,0 +1,22 @@
+import { useColorMode, IconButton, Tooltip } from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+
+export const DarkModeSwitch = () => {
+	const { colorMode, toggleColorMode } = useColorMode();
+	const isDark = colorMode === "dark";
+	const tooltipLabel = colorMode === "light" ? "ダークモードへ切り替えます" : "ライトモードへ切り替えます";
+	return (
+		<Tooltip label={tooltipLabel} placement={"right-end"}>
+			<IconButton
+				icon={isDark ? <SunIcon /> : <MoonIcon />}
+				aria-label="Toggle Theme"
+				colorScheme="green"
+				onClick={toggleColorMode}
+				position="absolute"
+				top={{ base: 2, md: 4 }}
+				right={{ base: 6, md: 8 }}
+				zIndex={1}
+			/>
+		</Tooltip>
+	);
+};
