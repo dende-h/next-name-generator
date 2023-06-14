73c806cb0787bd392bb20c8824d50b8f9fd48411
Hiroya Shitogiden <dende.capo@gmail.com>
2023-06-14 11:13:43 +0900

    import不備修正

--- src/components/ThemeGenerator/ThemeOutput.tsx
+++ src/components/ThemeGenerator/ThemeOutput.tsx
@@ -5,7 +5,6 @@ type ThemeOutputProps = {
     theme:string;
   }
   
-
 // 新たな ThemeOutput コンポーネント
 export const ThemeOutput:FC<ThemeOutputProps> = ({theme}) => (
   <Text color={useColorModeValue("gray.800","white")}>生成された小説のテーマ: {theme}</Text>
--- src/components/__test__/ThemeGenerator.test.tsx
+++ src/components/__test__/ThemeGenerator.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import ThemeGenerator from "../ThemeGenerator";
+import ThemeGenerator from "../ThemeGenerator/ThemeGenerator";
 
 
 test("renders ThemeGenerator component", () => {
