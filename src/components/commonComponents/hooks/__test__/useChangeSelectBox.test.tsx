46609e68f055e29a9e9af356982fa81b1faa43d4
Hiroya Shitogiden <dende.capo@gmail.com>
2023-06-16 10:45:22 +0900

    testコンポーネントファイルを削除してWrapperコンポーネントをテストファイル内に作成

--- src/components/commonComponents/hooks/__test__/useChangeSelectBox.test.tsx
+++ src/components/commonComponents/hooks/__test__/useChangeSelectBox.test.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { render, fireEvent, waitFor } from "@testing-library/react";
-import { TestSelectBox } from "./TestComponent";
+import { useChangeSelectBox } from "../useChangeSelectBox";
+import { Box, Select, Text } from "@chakra-ui/react";
+
+const Wrapper = () => {
+	const { selectValue, onChangeSelectValue } = useChangeSelectBox();
+
+	return (
+		<Box>
+			<Select placeholder={"選択してください"} onChange={onChangeSelectValue}>
+				<option value={"test1"}>テスト1</option>
+				<option value={"test2"}>テスト2</option>
+				<option value={"test3"}>テスト3</option>
+			</Select>
+			<Text>{selectValue ? selectValue : "undefined"}</Text>
+		</Box>
+	);
+};
 
 describe("useChangeSelectBoxのテスト", () => {
 	test("初期状態ではselectValueがundefinedであること", () => {
-		const { getByText } = render(<TestSelectBox />);
+		const { getByText } = render(<Wrapper />);
 		expect(getByText("undefined")).toBeInTheDocument();
 	});
 
 	test("selectの選択に応じてselectValueが更新されること", async () => {
-		const { getByText, getByRole } = render(<TestSelectBox />);
+		const { getByText, getByRole } = render(<Wrapper />);
 		const selectElement = getByRole("combobox");
 
 		fireEvent.change(selectElement, { target: { value: "test1" } });
